39af2ebbad98db610d13ab4b0c94a36fa57ff507
iswara.chaitanya <iswara@sivananda.org>
2021-04-16 18:27:19 -0400

    add cannot attend page and masters images

--- meeting-web/script.js
+++ meeting-web/script.js
@@ -8,5 +8,23 @@ function loadMain() {
   if (pathname.match(/interested$/)) {
     // @ts-ignore
     document.querySelector(".interested").style.display = "block";
+  } else if (pathname.match(/cannot-attend$/)) {
+    // @ts-ignore
+    document.querySelector(".cannot-attend").style.display = "block";
+    document
+      .querySelector(".cannot-attend .submit")
+      ?.addEventListener("click", submitResponse);
   }
+}
+
+/**
+ * @param {Event} e
+ */
+function submitResponse(e) {
+  const thankYou = document.querySelector(".thank-you-for-feedback");
+
+  // @ts-ignore
+  if (thankYou) thankYou.style.display = "block";
+  // @ts-ignore
+  e.target.setAttribute("disabled", true);
 }
