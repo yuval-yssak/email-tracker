965b2e7f7ff3afd312bac43c8f5501afa7ea64f3
iswara.chaitanya <iswara@sivananda.org>
2021-04-16 11:35:25 -0400

    add header and redirects rule

---
+++ meeting-web/script.js
@@ -0,0 +1,7 @@
+// @ts-check
+document.addEventListener("DOMContentLoaded", loadMain);
+
+function loadMain() {
+  const main = document.getElementById("main");
+  //   if (window.location.href )
+}
