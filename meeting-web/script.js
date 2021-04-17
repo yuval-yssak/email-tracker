ae6496ad736c815c02bdd73b3cb8c82e33358100
iswara.chaitanya <iswara@sivananda.org>
2021-04-16 20:24:16 -0400

    add mongoose to track both links and feedback

--- meeting-web/script.js
+++ meeting-web/script.js
@@ -6,25 +6,59 @@ function loadMain() {
   const { pathname } = new URL(window.location.href);
 
   if (pathname.match(/interested$/)) {
+    fetch(
+      `http://localhost:3030/${pathname
+        .replace(/\/interested$/, "")
+        .slice(1)}/interested`,
+      { method: "POST" }
+    );
+
+    const slot = document.querySelector(".interested");
+
     // @ts-ignore
-    document.querySelector(".interested").style.display = "block";
+    if (slot) slot.style.display = "block";
   } else if (pathname.match(/cannot-attend$/)) {
+    fetch(
+      `http://localhost:3030/${pathname
+        .replace(/\/cannot-attend$/, "")
+        .slice(1)}/cannot-attend`,
+      { method: "POST" }
+    );
+
+    const slot = document.querySelector(".cannot-attend");
+
     // @ts-ignore
-    document.querySelector(".cannot-attend").style.display = "block";
+    if (slot) slot.style.display = "block";
     document
       .querySelector(".cannot-attend .submit")
       ?.addEventListener("click", submitResponse);
   }
-}
 
-/**
- * @param {Event} e
- */
-function submitResponse(e) {
-  const thankYou = document.querySelector(".thank-you-for-feedback");
+  /**
+   * @param {Event} e
+   */
+  function submitResponse(e) {
+    fetch(
+      `http://localhost:3030/${pathname
+        .replace(/\/cannot-attend$/, "")
+        .slice(1)}/feedback`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          // @ts-ignore
+          feedback: document.querySelector(".cannot-attend textarea").value,
+        }),
+      }
+    );
 
-  // @ts-ignore
-  if (thankYou) thankYou.style.display = "block";
-  // @ts-ignore
-  e.target.setAttribute("disabled", true);
+    const thankYou = document.querySelector(".thank-you-for-feedback");
+
+    // @ts-ignore
+    if (thankYou) thankYou.style.display = "block";
+    // @ts-ignore
+    e.target.setAttribute("disabled", true);
+  }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,79 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
 const app = express();
+app.use(cors());
+
+mongoose.connect("mongodb://localhost/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const clickSchema = new mongoose.Schema({
+  email: String,
+  timestamp: Date,
+  link: String,
+});
+
+const Click = mongoose.model("Click", clickSchema);
+
+const feedbackSchema = new mongoose.Schema({
+  email: String,
+  timestemp: Date,
+  feedback: String,
+});
+const Feedback = mongoose.model("Feedback", feedbackSchema);
 
 app.get("/:id/small-image.png", (req, res) => {
   console.log(req.params.id);
   res.sendFile("1x1-00ffff7f.png", { root: "assets" });
 });
 
-app.listen(3030, () => console.log("listening on 3030"));
+app.post("/:email/interested", async (req, res) => {
+  const email = req.params.email;
+  const newClick = new Click({
+    email,
+    timestamp: Date.now(),
+    link: "interested",
+  });
+
+  await newClick.save();
+
+  res.send();
+});
+
+app.post("/:email/cannot-attend", async (req, res) => {
+  const email = req.params.email;
+  const newClick = new Click({
+    email,
+    timestamp: Date.now(),
+    link: "cannot-attend",
+  });
+
+  await newClick.save();
+
+  res.send();
+});
+
+const jsonParser = express.json();
+
+app.post("/:email/feedback", jsonParser, async (req, res) => {
+  const email = req.params.email;
+  const newClick = new Feedback({
+    email,
+    timestamp: Date.now(),
+    feedback: req.body.feedback,
+  });
+
+  await newClick.save();
+
+  res.send();
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  // we're connected!
+  app.listen(3030, () => console.log("listening on 3030"));
+});
